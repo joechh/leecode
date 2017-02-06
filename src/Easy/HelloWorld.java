6729e76d770621c2152b6406dbd6e64df3d713c8
Joe Hsu <joechh@pilottv.com.tw>
2017-02-06 17:18:44 +0800

    Testing

---
+++ src/Easy/HelloWorld.java
@@ -0,0 +1,10 @@
+package Easy;
+
+/**
+ * Created by joechh on 2017/2/6.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello~LC");
+    }
+}
