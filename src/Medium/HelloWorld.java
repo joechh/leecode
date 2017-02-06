a347d5ef1f169053856100f6ffc01df140d52ae1
Joe Hsu <joechh@pilottv.com.tw>
2017-02-06 17:24:41 +0800

    Testing Second Round

---
+++ src/Medium/HelloWorld.java
@@ -0,0 +1,10 @@
+package Medium;
+
+/**
+ * Created by joechh on 2017/2/6.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello~LC");
+    }
+}
